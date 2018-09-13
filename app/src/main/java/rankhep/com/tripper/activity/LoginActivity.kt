f2a9898e273e3b945bfcbb2e471904fa90ff4231
nonop1507 <nono1507@naver.com>
2018-09-13 22:18:34 +0900

    로그인 액티비티 생성 및 로그인 기능 추가 TODO : 로그인 후 유저데이터를 통한 메인 데이터 세팅

---
+++ app/src/main/java/rankhep/com/tripper/activity/LoginActivity.kt
@@ -0,0 +1,63 @@
+package rankhep.com.tripper.activity
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import org.json.JSONObject
+import rankhep.com.dhlwn.utils.NetworkHelper
+import rankhep.com.tripper.R
+import rankhep.com.tripper.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginBtn.setOnClickListener {
+            checkBlank(loginEmailEditText.text.toString(), loginPwdEditText.text.toString())
+        }
+    }
+
+    private fun checkBlank(email: String, pwd: String) {
+        when {
+            email == "" -> Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            pwd == "" -> Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            else -> sendRequest(email, pwd)
+        }
+
+    }
+
+    private fun sendRequest(email: String, pwd: String) {
+        var paramObject = JSONObject()
+        paramObject.put("email", email)
+        paramObject.put("password", pwd)
+        NetworkHelper.networkInstance
+                .loginUser(RequestBody.create(MediaType.parse("application/json"), paramObject.toString()))
+                .enqueue(object : Callback<User> {
+                    override fun onFailure(call: Call<User>?, t: Throwable?) {
+                        Toast.makeText(applicationContext, "${t?.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onResponse(call: Call<User>?, response: Response<User>?) {
+                        if (response?.code() == 200) {
+                            val intent = Intent()
+                            intent.putExtra("user", response.body())
+                            setResult(Activity.RESULT_OK, intent)
+                            finish()
+                        } else {
+                            Toast.makeText(applicationContext,"로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+    }
+}
--- app/src/main/java/rankhep/com/tripper/activity/MainActivity.kt
+++ app/src/main/java/rankhep/com/tripper/activity/MainActivity.kt
@@ -1,19 +1,33 @@
 package rankhep.com.tripper.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import rankhep.com.tripper.fragment.MainFragment
 import rankhep.com.tripper.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.title){
+            "로그인"->{
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivityForResult(intent, 333)
+            }
+        }
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         replaceFragment(MainFragment.newInstance())
+
+        navigation_view.setNavigationItemSelectedListener(this)
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -22,12 +36,14 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.fragment_container, fragment).commit()
     }
 
-    public fun openDrawer() {
+    fun openDrawer() {
         drawer_layout.openDrawer(GravityCompat.START)
 
     }
 
-    public fun closeDrawer() {
+    fun closeDrawer() {
         drawer_layout.closeDrawers()
     }
+
+
 }
