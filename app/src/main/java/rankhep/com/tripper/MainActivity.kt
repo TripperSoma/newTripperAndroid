ba2b3bc97b652fa4f29b2b73d641f3ac93024742
nonop1507 <nono1507@naver.com>
2018-09-13 21:40:20 +0900

    Initial commit

---
+++ app/src/main/java/rankhep/com/tripper/MainActivity.kt
@@ -0,0 +1,20 @@
+package rankhep.com.tripper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        replaceFragment(MainFragment.newInstance())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment).commit()
+    }
+}
