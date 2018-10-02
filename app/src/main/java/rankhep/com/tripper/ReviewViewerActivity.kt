752d9a7c0741f297e79f3caa04c5691b1e20ed25
nonop1507 <nono1507@naver.com>
2018-10-02 23:53:39 +0900

    리뷰 상세보기 페이지 xml작성

---
+++ app/src/main/java/rankhep/com/tripper/ReviewViewerActivity.kt
@@ -0,0 +1,12 @@
+package rankhep.com.tripper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewViewerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_viewer)
+    }
+}
