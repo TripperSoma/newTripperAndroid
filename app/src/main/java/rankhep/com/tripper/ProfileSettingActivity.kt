46a860c507832e268f32cf2d8cb813b63adcbeb5
nonop1507 <nono1507@naver.com>
2018-10-10 22:33:25 +0900

    프로필 변경 액티비티 생성

---
+++ app/src/main/java/rankhep/com/tripper/ProfileSettingActivity.kt
@@ -0,0 +1,14 @@
+package rankhep.com.tripper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileSettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_setting)
+
+
+    }
+}
