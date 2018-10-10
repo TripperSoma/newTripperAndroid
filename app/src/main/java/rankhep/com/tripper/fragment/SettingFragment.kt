7cb6bcca3915ab6fd2895598047db92187735b3d
nonop1507 <nono1507@naver.com>
2018-10-10 22:43:28 +0900

    파일정리 실수

--- app/src/main/java/rankhep/com/tripper/fragment/SettingFragment.kt
+++ app/src/main/java/rankhep/com/tripper/fragment/SettingFragment.kt
@@ -8,7 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_setting.view.*
-import rankhep.com.tripper.ProfileSettingActivity
+import rankhep.com.tripper.activity.ProfileSettingActivity
 import rankhep.com.tripper.R
 import rankhep.com.tripper.activity.MainActivity
 
